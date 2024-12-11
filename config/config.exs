240a7e4b9a5e710a3dc5bc18759c24cfc0a37790
Alex Moreira <alexbmoreira@gmail.com>
2024-12-11 13:05:22 -0500

    whoops lol

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,6 @@
 import Config
 
 config :tmdb_elixir,
-  # auth_token: System.get_env("TMDB_AUTH_TOKEN")
-  auth_token: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWI0ZWZjNTE4Yjc5YzVjZDdlMGMzMTE3NDU3MmMzNCIsIm5iZiI6MTU4OTkzMzUwMC40OTQwMDAyLCJzdWIiOiI1ZWM0NzViYzhlMmUwMDNkZDYxMGYyMDgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1EOKF7OItTLHufwkNgEVgLT1mPGyO3EUs8C9UiNmKhE"
+  auth_token: System.get_env("TMDB_AUTH_TOKEN")
 
 import_config "#{config_env()}.exs"
